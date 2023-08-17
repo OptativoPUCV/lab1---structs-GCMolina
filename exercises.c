0131ca11baa3fbe110574dfbbe8e6ae121e1b3ef
Replit user <>
2023-08-17 03:40:20 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -10,13 +10,16 @@ Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
 int findMax(int arr[], int size) {
-  int max = arr[size+1];
-  for (int i = 0; i < size; i++) {
-    if (arr[i] > max) {
-            max = arr[i];  
-      }
+  int copia = 0;
+  for(int i=0; i<size; i++){
+    if (arr[i]>copia){
+      copia=arr[i];
+        
+      
     }
-  return arr[0]; 
+  }
+  return copia;
+
 }
 
 /*
