24fab3add8ddad88568431711b0431683cf65005
Replit user <>
2023-08-17 04:14:41 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -42,6 +42,7 @@ y su tamaño, y luego devuelva un nuevo arreglo que contenga solo
 los números pares del arreglo original.
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize) {
+  *newSize=0;
   for (int i=0; i<size;i++){
     if (arr[i]%2==0){
       *newSize=*newSize+1;
